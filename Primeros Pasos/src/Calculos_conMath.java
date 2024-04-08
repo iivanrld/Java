f035856ab16c549ba3a7a77ee560d9c629b530bf
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-04-08 14:27:58 +0200

    save

--- Primeros Pasos/src/Calculos_conMath.java
+++ Primeros Pasos/src/Calculos_conMath.java
@@ -4,9 +4,20 @@ public class Calculos_conMath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double raiz=Math.sqrt(9);
+		//double raiz=Math.sqrt(9);
 		
-		System.out.println(raiz);
+		float num1=5.85F;
+		
+		int resultado1=Math.round(num1);
+		System.out.println(resultado1);
+		
+		double base=5;
+		
+		double exponente=3;
+		
+		int resultado2=(int)Math.pow(base, exponente);
+		
+		System.out.println("el resultado de "+ base + " elevado a " + exponente + " es " +resultado2);
 	}
 
 }
