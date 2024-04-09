fd2ccdcd9db5e1d23c6357de97e6bfc5e272dc44
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-04-09 14:07:04 +0200

    save

---
+++ Primeros Pasos/src/Manipula_Cadenas_II.java
@@ -0,0 +1,15 @@
+
+public class Manipula_Cadenas_II {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String frase="Hoy es un estupendo dia para apreder a programar en Java";
+		
+		String frase_resumen=frase.substring(0, 29) + " irnos a la play y olvidarnos de todo...." + " y " + frase.substring(29, 57);
+		
+		System.out.println(frase_resumen);
+
+	}
+
+}
